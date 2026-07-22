53da8099f5bf4e3d9e942c2fcb4097f0dcc5e172
Rahul Ambarakonda <35566693+rahul-ambarakonda@users.noreply.github.com>
2026-07-22 14:52:26 +0530

    feat: create src/App.js with a Chakra UI button

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, Box } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box p={4}>
+      <Button colorScheme="teal">Hello Chakra UI</Button>
+    </Box>
+  );
+}
+
+export default App;
