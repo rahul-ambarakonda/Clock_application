84c7500fd81a3a0cc3da0291e1153cc32b99db39
Rahul Ambarakonda <35566693+rahul-ambarakonda@users.noreply.github.com>
2026-07-22 14:52:50 +0530

    feat: update App.js to use Layout component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Button, Box } from '@chakra-ui/react';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <Box p={4}>
-      <Button colorScheme="teal">Hello Chakra UI</Button>
-    </Box>
+    <Layout>
+      <Box p={4}>
+        <Button colorScheme="teal">Hello Chakra UI</Button>
+      </Box>
+    </Layout>
   );
 }
 
